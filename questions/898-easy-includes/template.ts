27ef7a81782dae87514f8d29dfaa6752d1f39544
zhangy <openbraver@yeah.net>
2022-03-27 17:21:32 +0800

    complete: 898

--- questions/898-easy-includes/template.ts
+++ questions/898-easy-includes/template.ts
@@ -1,1 +1,7 @@
-type Includes<T extends readonly any[], U> = any;
+import { Equal } from '@type-challenges/utils'
+
+export type Includes<T extends readonly any[], U> = T extends [infer E, ...infer NT]
+  ? Equal<E, U> extends true
+    ? true
+    : Includes<NT, U>
+  : false
